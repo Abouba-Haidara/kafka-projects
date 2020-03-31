ec17cde4cdea49a9425bb0f4da28219ba22851e6
Yemini Coder <31940004+Abouba-Haidara@users.noreply.github.com>
2020-03-31 11:27:48 +0000

    change folder kafka project

---
+++ kafka.2/src/main/java/com/haidara/DemokafkaproducerconsumerApplication.java
@@ -0,0 +1,13 @@
+package com.haidara;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemokafkaproducerconsumerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemokafkaproducerconsumerApplication.class, args);
+    }
+
+}
---
+++ kafka.2/src/test/java/com/haidara/DemokafkaproducerconsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.haidara;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemokafkaproducerconsumerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
